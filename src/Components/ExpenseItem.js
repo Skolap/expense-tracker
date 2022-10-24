e3c8fc4747e60704cfb916c7c828f92e1682728b
Shailesh Kolap <skolap22@gmail.com>
2022-10-25 00:07:12 +0530

    Added CCS style to ExpenseItem Component

--- src/Components/ExpenseItem.js
+++ src/Components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import React from "react";
-
+import "./ExpenseItem.css";
 function ExpenseItem() {
   return (
-    <div>
+    <div className="expense-item">
       <div>Oct 23 2022</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Car Insurance</h2>
-        <div>₹50000</div>
+        <div className="expense-item__price">₹50000</div>
         <p></p>
       </div>
     </div>
