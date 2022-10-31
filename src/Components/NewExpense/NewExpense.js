1494410e569c00fd304cbdf9285dc1ed8c70ebf4
Shailesh Kolap <skolap22@gmail.com>
2022-10-31 12:13:21 +0530

    Complated State uplifting

--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -2,11 +2,16 @@ import React from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 function NewExpense(props) {
+  //Send data to app.js
   const saveExpanseDataHandler = (enteredExpanseData) => {
+    //Save data in object to use later
     const expanseData = {
       ...enteredExpanseData,
       id: Math.random().toString(),
     };
+
+    //State uplift
+    //send data to App.js Component
     props.onAddExpense(expanseData);
   };
   return (
