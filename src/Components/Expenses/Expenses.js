fc25e5432f828462ef595d2a42f59e7282251c0e
Shailesh Kolap <skolap22@gmail.com>
2022-11-02 11:03:02 +0530

    Used Map function to dynamically allocate data to ExpenseItem Components

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -6,8 +6,15 @@ import "./Expenses.css";
 function Expenses(props) {
   return (
     <Card className="expenses">
-      1
-      <ExpenseItem
+      {/* Used Map function to dynamically allocate data to ExpenseItem Components*/}
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+      {/* <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
         date={props.items[0].date}
@@ -26,7 +33,7 @@ function Expenses(props) {
         title={props.items[3].title}
         amount={props.items[3].amount}
         date={props.items[3].date}
-      />
+      /> */}
     </Card>
   );
 }
